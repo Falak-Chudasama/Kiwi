f61cfaa62bac041e8986e1c8abd1998eac21fa65
Falak-Chudasama <falakchudasama7766@gmail.com>
2026-08-29 22:01:17 +0530

    fix: update server and client configurations to use consistent localhost settings

--- server/run.py
+++ server/run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8420, reload=False)
+    uvicorn.run("app.main:app", host="localhost", port=5060, reload=False)
